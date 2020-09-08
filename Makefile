b0a997f2d6964f301e53a174bda94974d8ba94d2
Aaron Oman <groovestomp@groovestomp.com>
2020-09-08 15:36:24 -0700

    First commit

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 GITHUB_ORG := $(shell echo ${REPOSITORY} | cut -d "/" -f 2)
 GITHUB_REPO := $(shell echo ${REPOSITORY} | cut -d "/" -f 3)
 
-run: create-db-user
+run:
 	@echo "Set CIRCLECI environment variables\n"
 	export AWS_ACCESS_KEY_ID=$(shell aws secretsmanager get-secret-value --region ${region} --secret-id=ci-user-aws-keys${randomSeed} | jq -r '.SecretString'| jq -r .access_key_id)
 	export AWS_SECRET_ACCESS_KEY=$(shell aws secretsmanager get-secret-value --region ${region} --secret-id=ci-user-aws-keys${randomSeed} | jq -r '.SecretString'| jq -r .secret_key)
@@ -17,16 +17,8 @@ run: create-db-user
 	curl -X POST https://circleci.com/api/v1.1/project/github/${GITHUB_ORG}/${GITHUB_REPO}/follow?circle-token=${CIRCLECI_API_KEY}
 	@echo "\nDone"
 
-create-db-user:
-	export REGION=${region}; \
-	export SEED=${randomSeed}; \
-	export PROJECT_NAME=${PROJECT_NAME}; \
-	export ENVIRONMENT=${ENVIRONMENT}; \
-	export DATABASE=${database}; \
-	sh ./db-ops/create-db-user.sh
-
 summary:
-	@echo "zero-deployable-backend:"
+	@echo "zero-deployable-node-backend:"
 	@echo "- Repository URL: ${REPOSITORY}"
 	@echo "- Deployment Pipeline URL: https://app.circleci.com/pipelines/github/${GITHUB_ORG}/${GITHUB_REPO}"
 	@echo $(shell echo ${ENVIRONMENT} | grep prod > /dev/null && echo "- Production API: ${productionBackendSubdomain}${productionHostRoot}")
--- templates/app.js
+++ templates/app.js
@@ -1,11 +1,9 @@
-const PORT = 3000;
+var aws = require('aws-sdk');
+var cfsign = require('aws-cloudfront-sign');
+var express = require('express');
 
-const express = require('express');
-const app = express();
-
-const aws = require('aws-sdk');
-aws.config.update({ accessKeyId: process.env.ACCESS_KEY_ID, secretAccessKey: process.env.SECRET_ACCESS_KEY });
-const s3 = new aws.S3();
+var app = express();
+var s3 = new aws.S3();
 
 app.get('/presigned/:key', (req, res) => {
     var params = {
@@ -28,15 +26,39 @@ app.get('/presigned/:key', (req, res) => {
 
 app.get('/:key', (req, res) => {
     var params = {
-        Bucket: process.env.S3_BUCKET,
-        Key: req.params.key
+        keypairId: process.env.CF_KEYPAIR_ID,
+        privateKeyString: process.env.CF_KEYPAIR_SECRET_KEY,
+        expireTime: new Date().getTime() + 30000 // defaults to 30s
     };
 
-    var url = s3.getSignedUrl('getObject', params);
+    var url = cfsign.getSignedUrl(
+        `https://files.commit.dev/${req.params.key}`,
+        params
+    );
+
     console.log(url);
     res.redirect(url);
 });
 
-app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
+app.get('/status/ready', (req, res) => {
+    res.send("OK");
+});
+
+app.get('/status/alive', (req, res) => {
+    res.send("OK");
+});
+
+app.get('/status/about', (req, res) => {
+    res.send({
+        podName: process.env.POD_NAME
+    });
+});
+
+var port = process.env.PORT;
+if (!port) {
+    port = 3000;
+}
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
