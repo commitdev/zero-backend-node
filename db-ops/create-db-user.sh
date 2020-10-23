d815413867b2e8400b45e49de0ff65359a50a799
Steven Shi <sshi100@users.noreply.github.com>
2020-10-23 10:43:33 -0700

    rename ci-user (#7)
    * rename ci-user
    
    * set deployer role for ci and fix db-user creation
    
    * resume to number version 0.0.3

--- db-ops/create-db-user.sh
+++ db-ops/create-db-user.sh
@@ -13,19 +13,20 @@ DB_APP_USERNAME=$DB_NAME
 DB_APP_PASSWORD=$(LC_ALL=C tr -dc 'A-Za-z0-9' < /dev/urandom | base64 | head -c 16)
 
 # Fill in env-vars to db user creation manifest
+JOB_ID=$(LC_ALL=C tr -dc 'a-z0-9' < /dev/urandom | head -c 8)
 eval "echo \"$(cat ./db-ops/job-create-db-$DATABASE.yml.tpl)\"" > ./k8s-job-create-db.yml
 # the manifest creates 4 things
-# 1. Namespace: db-ops
+# 1. Namespace: db-ops 
 # 2. Secret in db-ops: db-create-users (with master password, and a .sql file
 # 3. Job in db-ops: db-create-users (runs the .sql file against the RDS given master_password from env)
-# 4. Secret in Application namespace with DB_USERNAME / DB_PASSWORD
+# 4. Secret in Application namespace with DB_USERNAME / DB_PASSWORD 
 kubectl apply -f ./k8s-job-create-db.yml
 
 # Deleting the entire db-ops namespace, leaving ONLY application-namespace's secret behind
-kubectl -n db-ops wait --for=condition=complete --timeout=10s job db-create-users
+kubectl -n db-ops wait --for=condition=complete --timeout=10s job db-create-users-${JOB_ID}
 if [ $? -eq 0 ]
-then
-  kubectl get namespace db-ops
-else
-  echo "Failed to create application database user, please see 'kubectl logs -n db-ops -l job-name=db-create-users'"
+then 
+  kubectl delete namespace db-ops
+else 
+  echo "Failed to create application database user, please see 'kubectl logs -n db-ops -l job-name=db-create-users-${JOB_ID}'"
 fi
