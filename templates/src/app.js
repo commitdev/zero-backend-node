089e204edb81d46ec2f745a497b9709e4722d0d3
David Cheung <davidcheung@live.ca>
2020-11-13 18:49:24 -0500

    add example auth-data to show in frontend

--- templates/src/app.js
+++ templates/src/app.js
@@ -58,6 +58,29 @@ app.get("/status/about", (req, res) => {
   });
 });
 
+<%if eq (index .Params `userAuth`) "yes" %>
+app.get('/auth-data', (req, res) => {
+  /** Expecting oathkeeper to pass on user identity in headers
+   *  (note nodejs converts all headers to lowercase)
+   *  1. X-User-Id
+   *  2. X-User-Email
+  * */
+  const hasKratosCookie = req.headers.cookie.match(/ory_kratos_session/);
+  const hasUserData = req.headers["x-user-id"] && req.headers["x-user-email"];
+  if (hasKratosCookie && hasUserData) {
+    res.json({
+      user_id: req.headers["x-user-id"],
+      email: req.headers["x-user-email"],
+    });
+  } else {
+    res.status(401);
+    res.json({
+      success: false,
+      message: "unauthenticated",
+    });
+  }
+});<% end %>
+
 var port = process.env.SERVER_PORT;
 if (!port) {
   port = 3000;
