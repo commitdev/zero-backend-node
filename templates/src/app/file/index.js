c6b3f9a85d5887e4246a7fbe214bbe062281daf3
Larry Lin <larrylin17@gmail.com>
2021-02-22 12:49:09 -0800

    update file apis to match Go version

--- templates/src/app/file/index.js
+++ templates/src/app/file/index.js
@@ -5,39 +5,32 @@ var cfsign = require("aws-cloudfront-sign");
 var router = Router();
 var s3 = new aws.S3();
 
-router.get("/presigned/:key", (req, res) => {
+router.get("/presigned", (req, res) => {
   var params = {
-    Bucket: process.env.S3_BUCKET,
-    Fields: {
-      key: req.params.key,
-    },
+    Bucket: process.env.AWS_S3_DEFAULT_BUCKET,
+    Key: req.query.key,
+    Expires: 60 * 5, // 5 minutes
   };
-
-  s3.createPresignedPost(params, (err, data) => {
-    if (err) {
-      console.error(err);
-      res.sendStatus(500);
-    } else {
-      console.log(data);
-      res.send(data);
-    }
+  const url = s3.getSignedUrl('putObject',params);
+  console.log(url);
+  res.send({
+    "url":url,
+    "method":"put"
   });
 });
 
-router.get("/:key", (req, res) => {
+router.get("/",(req, res) => {
   var params = {
-    keypairId: process.env.CF_KEYPAIR_ID,
-    privateKeyString: process.env.CF_KEYPAIR_SECRET_KEY,
-    expireTime: new Date().getTime() + 30000, // defaults to 30s
+    Bucket: process.env.AWS_S3_DEFAULT_BUCKET,
+    Key: req.query.key,
+    Expires: 60 * 5, // 5 minutes
   };
-
-  var url = cfsign.getSignedUrl(
-    `https://files.${process.env.DOMAIN}/${req.params.key}`,
-    params
-  );
-
+  const url = s3.getSignedUrl('getObject',params);
   console.log(url);
-  res.redirect(url);
+  res.send({
+    "url":url,
+    "method":"get"
+  });
 });
 
 module.exports = router;
