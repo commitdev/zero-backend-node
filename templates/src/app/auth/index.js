1eae7e2bec4b27e3881efe4f9bed3ab05fdf5fcd
Larry Lin <larrylin17@gmail.com>
2021-02-22 16:49:29 -0800

    add apiType="graphql/rest" to package.json

--- templates/src/app/auth/index.js
+++ templates/src/app/auth/index.js
@@ -2,7 +2,7 @@ var { Router } = require("express");
 
 var { authMiddleware } = require("../../middleware/auth");
 
-var router = Router()
+var router = Router();
 
 router.get("/userInfo", authMiddleware, (req, res) => {
   res.json(req.user);
