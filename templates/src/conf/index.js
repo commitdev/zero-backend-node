713ae1eb79a05261665bb7fac83f407d022eff09
Larry Lin <larrylin17@gmail.com>
2021-03-05 10:47:12 -0800

    change download link from s3 presigned url to cloudfront signed url.

--- templates/src/conf/index.js
+++ templates/src/conf/index.js
@@ -1,5 +1,7 @@
 const conf = {
-    jwtSecret: "SecretfromIdentityProvider"
+    jwtSecret: "SecretfromIdentityProvider",
+    s3PresignedExpires: 60 * 5, //5 minutes 
+    cfSignerExpires: 1000 * 60 * 5 //5 minutes
 }
 
 module.exports = conf;
--- templates/src/service/file.js
+++ templates/src/service/file.js
@@ -1,4 +1,5 @@
 const aws = require("aws-sdk");
+const conf = require("../conf");
 
 const s3 = new aws.S3();
 
@@ -9,7 +10,7 @@ class FileService {
         const params = {
             Bucket: process.env.S3_BUCKET,
             Key: key,
-            Expires: 60 * 60, // 60 minutes
+            Expires: conf.s3PresignedExpires,
         };
         const url = s3.getSignedUrl('putObject',params);
         return {
@@ -19,13 +20,17 @@ class FileService {
     }
 
     getDownloadPresignedUrl(key){
-        const params = {
-            Bucket: process.env.S3_BUCKET,
-            Key: key,
-            Expires: 60 * 60, // 60 minutes
-        };
-        const url = s3.getSignedUrl('getObject',params);
-        console.log(url);
+        const cloudfrontAccessKeyId = process.env.CF_KEYPAIR_ID;
+        const cloudFrontPrivateKey = process.env.CF_KEYPAIR_SECRET_KEY;
+        const cloudFrontDomain = process.env.CF_DOMAIN;
+        const signer = new aws.CloudFront.Signer(cloudfrontAccessKeyId, cloudFrontPrivateKey);
+        key = (key.substring(0,1)=='/') ? key : "/"+key
+
+        const url = signer.getSignedUrl({
+            url: cloudFrontDomain+key,
+            expires: new Date().getTime() + conf.cfSignerExpires
+        });
+
         return {
             url: url,
             method: "get"
