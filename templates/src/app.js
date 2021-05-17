5f551b1a7ba8bd36c04ba58561bc8f3fd61851ad
David Cheung <davidcheung@live.ca>
2021-05-17 13:50:33 -0400

    Stripe initial impl (#27)
    * stripe iniitial implementation

--- templates/src/app.js
+++ templates/src/app.js
@@ -2,10 +2,14 @@ const dotenv = require("dotenv");
 const express = require("express");
 const morgan = require("morgan");
 const dbDatasource = require("./db");
-<%if eq (index .Params `fileUploads`) "yes" %>const fileRoutes = require("./app/file");<% end %>
-<%if eq (index .Params `userAuth`) "yes" %>const authRoutes = require("./app/auth");
-const { authMiddleware } = require("./middleware/auth");<% end %>
-const statusRoutes = require("./app/status");
+<%if eq (index .Params `fileUploads`) "yes" %>const fileRoutes = require("./app/file");
+<%- end %>
+<%- if eq (index .Params `userAuth`) "yes" %>const authRoutes = require("./app/auth");
+const { authMiddleware } = require("./middleware/auth");
+<%- end %>
+<%- if eq (index .Params `billingEnabled`) "yes" %>const billingRoutes = require("./app/billing");
+<%- end %>
+const publicRoutes = require("./app/public");
 
 dotenv.config();
 const app = express();
@@ -13,17 +17,15 @@ app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-<%if eq (index .Params `userAuth`) "yes" %>app.use(authMiddleware);
+// these are placed before the auth middleware, so will not 401 upon non-authenticated requests
+app.use("/", publicRoutes);
+
+<% if eq (index .Params `userAuth`) "yes" %>app.use(authMiddleware);
 app.use("/auth", authRoutes);<% end %>
+<% if eq (index .Params `fileUploads`) "yes" %>app.use("/file", fileRoutes);<% end %>
+<% if eq (index .Params `billingEnabled`) "yes" %>app.use("/billing", billingRoutes);<% end %>
 
-<%if eq (index .Params `fileUploads`) "yes" %>app.use("/file", fileRoutes);<% end %>
-
-app.use("/status", statusRoutes);
-
-const port = process.env.SERVER_PORT;
-if (!port) {
-  port = 3000;
-}
+const port = process.env.SERVER_PORT || 8080;
 
 const main = async () => {
   // remove this block for development, just for verifying DB
---
+++ templates/src/app/public/index.js
@@ -0,0 +1,24 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.get("/status/ready", (req, res) => {
+  res.json({ ready: "OK" });
+});
+
+router.get("/status/alive", (req, res) => {
+  res.json( {alive: "OK"} );
+});
+
+router.get("/status/about", (req, res) => {
+  var podName = (process.env.POD_NAME)?process.env.POD_NAME:"zero-node-backend";
+  res.json({
+    podName: podName,
+  });
+});
+<% if eq (index .Params `billingEnabled`) "yes" %>
+// Logging out webhooks for development purposes
+router.use("/webhook/stripe", (req, res) => {
+  res.json({ success:true });
+});<% end %>
+module.exports = router;
